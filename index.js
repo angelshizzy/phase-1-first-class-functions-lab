82b9e0cb5041070f91829f4ff0f6dbb2c667c9b4
Angela Shirao <angela.shirao@student.moringaschool.com>
2023-10-10 22:25:30 +0300

    Competed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+const drivers = ['Antonio', 'Nuru','Amari','Mo']
+const returnFirstTwoDrivers = (drivers) => {
+return drivers.slice (0,2);
+};
+
+const returnLastTwoDrivers = (drivers) => {
+    return drivers.slice (-2);
+};
+
+const selectingDrivers = ( returnFirstTwoDrivers, returnLastTwoDrivers);
